1b8a6862ffcd5c3930d2a703ff662796b6e05a53
AlanRms3101QaYDev <alanperez3101@gmail.com>
2022-07-09 00:57:52 -0500

    PO3 pagina de prueba 3, se mapean los campos

--- TestAutomationALAN/PageObject3/VuelosAUTpage.cs
+++ TestAutomationALAN/PageObject3/VuelosAUTpage.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using System;
 using System.Threading;
 using TestAutomationALAN.Handler;
@@ -13,10 +14,18 @@ namespace TestAutomationALAN.PageObject3
         protected By Desde = By.Id("from");//Se necesita agregar la posicion en la lista, cuando se utilice
         protected By ADonde = By.Id("To");//se necesita agregar la posicon del objeto en la lista
         protected By SalidaFecha = By.Id("//*[@id='mainpanel']/div[2]/form/div[2]/div[3]/div");//El formato que sigue es MM/DD/YYYY se le envia como texto 
-        protected By RegrespFecha = By.XPath("//*[@id='mainpanel']/div[2]/form/div[2]/div[4]/div");//El formato que sigue es MM/DD/YYYY Se le envia como texto 
+        protected By RegrespFecha = By.XPath("document.querySelector('#date')");//El formato que sigue es MM/DD/YYYY Se le envia como texto 
         protected By ClaseViaje = By.Id("classId");//Se modifica mediante la posicion del objeto en la lista
         protected By Pasajeros = By.XPath("//*[@id='passenger']");//Cantidad de pasajeros se busca mediante una lista de objetos
         protected By BtnBuscarViaje = By.Id("//*[@id='mainpanel']/div[2]/form/div[4]/div/button");//Botón de buscar viaje
+        protected By ValidacionBusquedaUndefined = By.XPath("//*[@id='content']/article/div/div/div/div/section/div/div/div/div/div/div/div/div/div/div[1]/div[2]");
+
+        protected By Regreso = By.XPath("//*[@id='mainpanel']/div[2]/form/div[2]/div[4]/label");
+
+        public VuelosAUTpage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
 
         /*Pagina para los casos de prueba una vez teniendo mapeados los objetos 
         
@@ -41,6 +50,7 @@ namespace TestAutomationALAN.PageObject3
             if( idavuelta != null) { 
             WaitHandler.ElementIsPresent(Driver, IdaVuelta);
             Driver.FindElement(IdaVuelta).Click();
+               
             }
 
             else {
@@ -49,6 +59,77 @@ namespace TestAutomationALAN.PageObject3
                 Console.WriteLine("No se encontraron los objetos");
                 
             }
+        }
+
+        public void BuscarViajeBtn()
+        {
+            Thread.Sleep(3000);
+            WaitHandler.ElementIsPresent(Driver, BtnBuscarViaje);
+            Driver.FindElement(BtnBuscarViaje).Click();
+        }
+
+        public void ClaseDeViaje( string clasedeviaje)
+        {
+            WaitHandler.ElementIsPresent(Driver, ClaseViaje);
+            Driver.FindElement(ClaseViaje).SendKeys(clasedeviaje);
+        }
+
+        public void CantPasajeros( string pasajeros)
+        {
+            WaitHandler.ElementIsPresent(Driver, Pasajeros);
+            Driver.FindElement(Pasajeros).SendKeys(pasajeros);
+        }
+
+        public void FechaSal(string FechaSal)
+        {
+            WaitHandler.ElementIsPresent(Driver, SalidaFecha);
+            Driver.FindElement(SalidaFecha).SendKeys(FechaSal);
+        }
+       
+        public void FechaReg( string FechaReg)
+        {
+          //  WaitHandler.ElementIsPresent(Driver,RegrespFecha);
+            //Driver.FindElement(RegrespFecha).SendKeys(FechaReg);
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+            js.ExecuteScript("arguments[1].click();",RegrespFecha);
+            RegrespFecha.ToString();
+            if (RegrespFecha != null)
+            {
+                Console.WriteLine(RegrespFecha.ToString());
+            }
+         
+        }
+
+        public void FiltroVuelos(string Redondo , string salida, string regreso, string claaase, string cantidadpasajeros)
+        {
+            if (Redondo == null)
+            {
+                WaitHandler.ElementIsPresent(Driver, SoloIda);
+                Driver.FindElement(SoloIda).Click();
+            }
+            if (Redondo != null)
+            {
+                WaitHandler.ElementIsPresent(Driver, IdaVuelta);
+                Driver.FindElement(IdaVuelta).Click();
+
+            }
+
+            else
+            {
+
+
+                Console.WriteLine("No se encontraron los objetos");
+
+            }
+            WaitHandler.ElementIsPresent(Driver, SalidaFecha);
+            Driver.FindElement(SalidaFecha);
+            WaitHandler.ElementIsPresent(Driver, RegrespFecha);
+            Driver.FindElement(RegrespFecha);
+            WaitHandler.ElementIsPresent(Driver, ClaseViaje);
+            Driver.FindElement(ClaseViaje);
+            WaitHandler.ElementIsPresent(Driver, Pasajeros);
+            Driver.FindElement(Pasajeros);
+
         }
     }
 }
---
+++ TestAutomationALAN/TestCase/Test3.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TestAutomationALAN.PageObject3;
+
+namespace TestAutomationALAN.TestCase
+
+{
+    [TestFixture]
+    public class Test3
+    {
+
+        public IWebDriver driver;
+
+
+        [SetUp]
+
+        public void BeforeTest()
+        {
+            driver = new ChromeDriver(@"C:\Users\alanp\Downloads\chromedriver_win32 (2)");
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(" https://www.testfaceclub.com/aut-vuelos/");
+        }
+
+
+
+
+        [Test]
+
+        public void FlightAround()
+        {
+            try
+            {
+                driver.SwitchTo().DefaultContent();
+ 
+                VuelosAUTpage vuelosAutPage = new VuelosAUTpage(driver);
+
+                vuelosAutPage.BuscarViajeBtn();
+                
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+    }
+}
+
